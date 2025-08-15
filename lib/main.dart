93cc091345a6bf1e4b19dc20b74c3480e2bcfbae
Samuel A. Fernandes <zamuelfernandes@gmail.com>
2025-08-15 10:08:34 -0300

    Conectando Google Cloud para login com google

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart' show ProviderScope;
+import 'package:google_sign_in/google_sign_in.dart';
 
 import 'cinebox_main_app.dart' show CineboxMainApp;
+import 'config/env.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  GoogleSignIn.instance.initialize(
+    serverClientId: Env.googleApiKey,
+  );
+
   runApp(
     ProviderScope(
       child: const CineboxMainApp(),
